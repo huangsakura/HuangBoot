0b9a518ce41bdb12e84ee620c4e92edc53bd9c95
huangjinlong <kmhd193623blc>
2017-11-15 10:29:53 +0800

    初始版本

---
+++ src/main/java/com/huanggit/HuangbootApplication.java
@@ -0,0 +1,12 @@
+package com.huanggit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuangbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuangbootApplication.class, args);
+	}
+}
