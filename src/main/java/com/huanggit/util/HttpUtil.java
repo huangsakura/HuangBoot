5b3c5ffa688d169f821d6a270824bc74bc6b49ad
1612740549@qq.com <kmhd193623blc>
2017-11-30 22:03:55 +0800

    提交

--- src/main/java/com/huanggit/util/HttpUtil.java
+++ src/main/java/com/huanggit/util/HttpUtil.java
@@ -6,10 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 /**
  * Created by huang on 2017-11-29-0029.
@@ -20,7 +22,14 @@ public class HttpUtil {
 
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
     private static final String ACCEPT = "text/html,application/xhtml+xml,application/xml,application/json";
+    private static final String ACCEPT_LANGUAGE = "en-US,en,zh-CN,zh";
+    private static final String CONTENT_TYPE = "application/x-www-form-urlencoded;charset=UTF-8;application/json";
 
+    /**
+     * http get
+     * @param link
+     * @return
+     */
     public static HttpResult get(String link) {
         HttpResult httpResult = new HttpResult();
         try {
@@ -29,15 +38,68 @@ public class HttpUtil {
             httpsURLConnection.setRequestMethod("GET");
             httpsURLConnection.setRequestProperty("User-Agent", USER_AGENT);
             httpsURLConnection.setRequestProperty("Accept", ACCEPT);
+            httpsURLConnection.setRequestProperty("Accept-Language",ACCEPT_LANGUAGE);
+            httpsURLConnection.setRequestProperty("content-type",CONTENT_TYPE);
             httpResult.setResponseCode(httpsURLConnection.getResponseCode());
             BufferedReader in = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
-            String inputLine;
+            String inputLine = null;
             StringBuffer response = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
             httpResult.setResponseText(response.toString());
+        } catch (MalformedURLException e) {
+            log.error("请求{}报错MalformedURLException:{}",link,e.getMessage());
+            httpResult.setErrorMessage(e.getMessage());
+        } catch (IOException e) {
+            log.error("请求{}报错IOException:{}",link,e.getMessage());
+            httpResult.setErrorMessage(e.getMessage());
+        } catch (Exception e) {
+            log.error("请求{}报错Exception:{}",link,e.getMessage());
+            httpResult.setErrorMessage(e.getMessage());
+        }
+        return httpResult;
+    }
+
+    /**
+     * http post
+     * @param link
+     * @param paramMap
+     * @return
+     */
+    public static HttpResult post(String link,Map<String,String> paramMap) {
+        HttpResult httpResult = new HttpResult();
+        try {
+            URL url = new URL(link);
+            HttpsURLConnection httpsURLConnection = (HttpsURLConnection)url.openConnection();
+            httpsURLConnection.setRequestMethod("POST");
+            httpsURLConnection.setRequestProperty("User-Agent", USER_AGENT);
+            httpsURLConnection.setRequestProperty("Accept", ACCEPT);
+            httpsURLConnection.setRequestProperty("Accept-Language",ACCEPT_LANGUAGE);
+            httpsURLConnection.setRequestProperty("content-type",CONTENT_TYPE);
+
+            httpsURLConnection.setDoOutput(true);
+            DataOutputStream dataOutputStream = new DataOutputStream(httpsURLConnection.getOutputStream());
+            StringBuffer stringBuffer = new StringBuffer();
+            for (Map.Entry<String,String> entry : paramMap.entrySet()) {
+                stringBuffer.append(entry.getKey() + "=" + entry.getValue() + "&");
+            }
+            dataOutputStream.writeBytes(stringBuffer.toString());
+            dataOutputStream.flush();
+            dataOutputStream.close();
+
+            httpResult.setResponseCode(httpsURLConnection.getResponseCode());
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
+            String inputLine = null;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            httpResult.setResponseText(inputLine.toString());
         } catch (MalformedURLException e) {
             log.error("请求{}报错MalformedURLException:{}",link,e.getMessage());
             httpResult.setErrorMessage(e.getMessage());
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,13 +1,15 @@
 <!doctype html>
 <html>
 <head>
-
+    <link rel="stylesheet" href="/webjars/jquery-ui/1.12.1/jquery-ui.min.css" />
+    <link rel="stylesheet" href="/webjars/jquery-ui/1.12.1/jquery-ui.theme.min.css" />
 </head>
 <body>
-
+    <input type="text" name="date" id="date" />
 </body>
 <script src="/webjars/jquery/1.12.0/jquery.min.js"></script>
+<script src="/webjars/jquery-ui/1.12.1/jquery-ui.min.js"></script>
 <script>
-
+    $("#date").datepicker();
 </script>
 </html>
