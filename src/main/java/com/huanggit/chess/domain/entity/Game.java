54bd50aa8ba3d9001de08923840fc15bd4858cc7
huangjinlong <1612740549@qq.com>
2020-06-29 19:30:16 +0800

    提交

--- src/main/java/com/huanggit/chess/domain/entity/Game.java
+++ src/main/java/com/huanggit/chess/domain/entity/Game.java
@@ -3,25 +3,36 @@ package com.huanggit.chess.domain.entity;
 import com.huanggit.chess.enumeration.Army;
 import com.huanggit.chess.enumeration.GameStatus;
 import com.huanggit.domain.entity.base.BaseEntity;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
  * Created by huang on 2018-05-03-0003.
  */
-@Data
+@Setter
+@Getter
+@ToString
 public class Game extends BaseEntity {
 
+    @NotNull
     private Long redUserId;
 
+    @NotNull
     private Long blackUserId;
 
+    @NotNull
     private GameStatus status;
 
+    @NotNull
     private Army winner;
 
+    @NotNull
     private Date startTime;
 
+    @NotNull
     private Date finishTime;
 }
